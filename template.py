8314b4d47bf7f8513a9d77e9b2a790c6215828a9
Marshall Bockrath <llasram@gmail.com>
2024-12-01 12:14:14 -0500

    Add basic template.

---
+++ template.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+import sys
+import fileinput
+
+
+def main(argv=sys.argv):
+    for line in fileinput.input(encoding="utf-8"):
+        pass
+    return
+
+
+if __name__ == '__main__':
+    main()
